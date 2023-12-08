fa3357dc5183f7242daed25193049d2a515597b7
Vlad-George Irimia <vlad-george.irimia@adesso-romania.ro>
2023-12-08 16:13:44 +0200

    added dependency project

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC;
+
+public class DependencyContainer
+{
+    public static void RegisterServices(IServiceCollection services)
+    {
+        //domain
+        services.AddTransient<IEventBus, RabbitMqBus>();
+    }
+}
