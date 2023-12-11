f16a06d982efbc3821e78df59b20d2eac61fff74
Vlad-George Irimia <vlad-george.irimia@adesso-romania.ro>
2023-12-11 11:55:22 +0200

    banking service

--- MicroRabbit.Banking.Api/Program.cs
+++ MicroRabbit.Banking.Api/Program.cs
@@ -1,36 +1,64 @@
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
+using Microsoft.EntityFrameworkCore;
 
-namespace MicroRabbit.Banking.Api
+namespace MicroRabbit.Banking.Api;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(cfg =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            cfg.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                Title = "Banking MicroService",
+                Version = "v1"
+            });
+        });
+        builder.Services.AddMediatR(cfg => 
+        {
+            cfg.RegisterServicesFromAssemblyContaining(typeof(Program));
+        });
 
-            app.UseHttpsRedirection();
+        builder.Services.AddDbContext<BankingDbContext>(opt =>
+        {
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("BankingDbConnection"));
+        });
 
-            app.UseAuthorization();
+        RegisterServices(builder.Services);
 
+        var app = builder.Build();
 
-            app.MapControllers();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(cfg =>
+            {
+                cfg.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking MicroService V1");
+            });
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+
+    private static void RegisterServices(IServiceCollection services)
+    {
+        DependencyContainer.RegisterServices(services);
     }
 }
--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,6 +1,12 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace MicroRabbit.Infra.IoC;
 
@@ -8,7 +14,20 @@ public class DependencyContainer
 {
     public static void RegisterServices(IServiceCollection services)
     {
-        //domain
+        // MediatR
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        // Domain Bus
         services.AddTransient<IEventBus, RabbitMqBus>();
+
+        // Applicaiton services
+        services.AddTransient<IAccountService, AccountService>();
+
+        // Data
+        services.AddTransient<IAccountRepository, AccountRepository>();
+        services.AddTransient<BankingDbContext>();
     }
 }
